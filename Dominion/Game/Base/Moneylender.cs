a16c63a487c4b9eb4f4e6b67d1471d8601aed5e6
Mathieu Fenniak <biziqe@mathieu.fenniak.net>
2011-02-22 20:18:30 -0700

    Add Moneylender

---
+++ Dominion/Game/Base/Moneylender.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dominion.Engine;
+
+namespace Dominion.Game.Base
+{
+    public class Moneylender : IActionCard
+    {
+        #region IActionCard Members
+
+        void IActionCard.Play(Dominion.Engine.Game game, Player player, Turn turn, object sidedata)
+        {
+            Copper copper = sidedata as Copper;
+            if (copper == null)
+                throw new Exception("Moneylender needs a copper");
+
+            player.TrashCard(copper);
+            turn.AddTreasure(3);
+        }
+
+        #endregion
+
+        #region ICard Members
+
+        public Enum CardEnum
+        {
+            get { return Cards.Moneylender; }
+        }
+
+        public CardType Type
+        {
+            get { return CardType.Action; }
+        }
+
+        public string Name
+        {
+            get { return "Moneylender"; }
+        }
+
+        public int Cost
+        {
+            get { return 4; }
+        }
+
+        #endregion
+    }
+}
